360ed628fc4dd5d2acbe540795913b50fb7a40ff
kavi <vettrithamil@gmail.com>
2024-09-17 22:57:41 +0530

    removed the logo

--- src/presentation/components/HomeSideDraw/home_side_draw.js
+++ src/presentation/components/HomeSideDraw/home_side_draw.js
@@ -4,8 +4,7 @@ import './home_side_draw.css';
 export default function HomeSideDraw(){
     return <div className="home-left-draw" >
     <div className='home-left-draw-logo' >
-        <img className='home-screen-logo' src='/assets/svg/hexaware-logo.svg'/>
-        <p> Hexaware <br/> Assignment</p>
+        <p className='home-left-draw-logo-text' > Hexaware <br/> Assignment</p>
     </div>
     <div className='home-left-draw-sections'>
         <div className='draw-section' >
--- src/presentation/pages/authentication/login.js
+++ src/presentation/pages/authentication/login.js
@@ -6,7 +6,6 @@ export default function LoginPage() {
     return (
         <div className="login-screen-wrapper" >
             <div className="login-screen-logo-heading" >
-              <img className="login-screen-logo" src="/assets/svg/hexaware-logo.svg" ></img>
               <h1 className="login-form-heading" >Hexaware<br />Assignment</h1>
             </div>
             <LoginForm />
